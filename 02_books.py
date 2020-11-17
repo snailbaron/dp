682f4c21a94d1d7842f0f8b4d82afcc489b6954e
snailbaron <snailbaron@gmail.com>
2020-11-17 06:41:59 +0300

    Add 02

--- 02_books.py
+++ 02_books.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
 
+years = [int(s) for s in input().split()]
+
+l = [0] * len(years)
+for i in range(len(years)):
+    l[i] = 1
+    for j in range(i):
+        if years[j] <= years[i] and l[j] + 1 > l[i]:
+            l[i] = l[j] + 1
+print(len(years) - max(l))
